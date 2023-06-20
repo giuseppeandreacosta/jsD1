b4b9d9c608964c1e62f492ddeeebc8ef1ac0ccd5
giuseppeandreacosta <giuseppeandreac@gmail.com>
2023-06-20 22:27:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Cos'è un algoritmo?
+
+// Un algortimo è una lista di istruzioni da seguire per risolvere un problema. 
+// Ad esempio, l’algoritmo per costruire un modellino Lego sono le istruzioni.
+
+
+// Cos'è una variabile?
+
+// Una variabile  è un contenitore di dati situato in una porzione di memoria destinata a contenere valori.
+// possibili di modifica nel corso dell’esecuzione di un programma.
+// in pratica una variabile è un’entità che può assumere diversi valori all’interno di un insieme.
+
+//Esempio :
+
+//          let motocicloKawasaki = 750
+//          
+
+
+
+
+
+
+// null e undefined sono la stessa cosa?
+
+// No, non sono la stessa cosa.
+
+//null : indica che alla variabile non è stato assegnato deliberatamente un valore.
+
+// let motociclo = null
+
+//undefined : indica, invece, una variabile non inizializzata, cioè a cui non è stato ancora assegnato un valore.
+
+// let motociclo
